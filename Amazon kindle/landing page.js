431343d2595ae1b8438e950c78fadfd4329cd480
UMlonzi <99178731+UMlonzi@users.noreply.github.com>
2022-05-19 15:10:32 +0200

    Add files via upload

--- Amazon kindle/landing page.js
+++ Amazon kindle/landing page.js
@@ -1,28 +1,15 @@
 let currentScrollPosition = 0;
-             let scrollAmount = 400;
-             const  sCont = document.querySelector(".storys-container");
-             const hScroll = document.querySelector(".horizontal-scroll");
-             const btnScrollLeft = document.querySelector("#btn-scroll-left");
-             const btnScrollRight = document.querySelector("#btn-scroll-right");
-             btnScrollLeft.style.opacity = "0";
-            
-             let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
-              function scrollHorizontally(val){
-                  currentScrollPosition += (val * scrollAmount);
-                  sCont.style.left = currentScrollPosition + "px";
-                  if(currentScrollPosition >= 0){
-                       currentScrollPosition = 0
-                       btnScrollLeft.style.opacity = "0";
-                  }
-                  else{
-                    btnScrollLeft.style.opacity = "1";
-                  }
-                  if(currentScrollPosition <= maxScroll){
-                      currentScrollPosition = maxScroll;
-                      btnScrollRight.style.opacity = "0";
-                  }
-                  else{
-                    btnScrollRight.style.opacity = "1";
-                  }
-                  sCont.style.left=currentScrollPosition + "px"; 
-              }
+let scrollAmount = 320;
+const sCont = document.querySelector(".story-container");
+const hScroll = document.querySelector(".horizontal-scroll");
+let maxScroll = -sCont.offserWidth + hScroll.offserWidth;
+function ScrollHorizontally(val){
+  currentScrollPosition += (val * scrollAmount);
+  if(currentScrollPosition > 0){
+  currentScrollPosition = 0;
+}
+if(currentScrollPosition > 0){
+  currentScrollPosition = maxScroll;
+}
+sCont.style.left = currentScrollPosition + "px";
+}
